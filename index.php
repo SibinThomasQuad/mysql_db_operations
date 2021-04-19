246acb8d0c2bdef93ce2e9e226fe93dfbfd3f73e
oakscode@gmail.com <oakscode@gmail.com>
2021-04-19 23:23:24 +0530

    push from sibin thomas @19-04-2021

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
-include('config.php');
 DB::insert('login',array('name'=>'sibin','pass'=>'123'),DB::init());
 ?>
+<select>
+<?php DB::FillDropdown('pass','name','login',DB::init()); ?>
+</select>
